938454f89b15d98483649731341995d0424265a2
Claudinei Aparecido Perboni <cperbony@gmail.com>
2020-01-15 22:01:34 -0300

    spec_helper config

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
 require 'calculator'
 
 describe Calculator, "Testes Calculadora" do
+  # subject(:subject) { describe_class.new() }
 
-	# subject(:subject) { describe_class.new() }
+  context '#sum' do
+    it 'with positive numbers' do
+      result = subject.sum(5, 7)
+      expect(result).to(eq(12))
+      # expect(result).not_to eq(1)
+    end
 
-	context '#sum' do
-		it 'with positive numbers' do
-			result = subject.sum(5, 7)
-			expect(result).to eq(12)
-			# expect(result).not_to eq(1)
-		end
+    it 'with negative and positive numbers' do
+      result = subject.sum(-5, 7)
+      expect(result).to(eq(2))
+    end
 
-		it 'with negative and positive numbers' do
-			result = subject.sum(-5, 7)
-			expect(result).to eq(2)
-		end
-
-			it 'with negative numbers' do
-			result = subject.sum(-5, -7)
-			expect(result).to eq(-12)
-		end
-	end
-	end
+    it 'with negative numbers' do
+      result = subject.sum(-5, -7)
+      expect(result).to(eq(-12))
+    end
+  end
+end
