505e2f3cc6add25ce0a9bce9ccdc9ae53cf1e791
Claudinei Aparecido Perboni <cperbony@gmail.com>
2020-01-20 11:22:41 -0300

    include matchers fixes

--- spec/matchers/comparacao/comparacao_spec.rb
+++ spec/matchers/comparacao/comparacao_spec.rb
@@ -1,27 +1,27 @@
+# frozen_string_literal: true
 describe "Matchers de Comparacao" do
   it ">" do
-    expect(5).to be > 1
+    expect(5).to(be > 1)
   end
 
   it ">=" do
-	expect(5).to be >= 2
-	expect(5).to be >= 5
+    expect(5).to(be >= 2)
+    expect(5).to(be >= 5)
   end
 
   it "<" do
-	expect(5).to be < 12
+    expect(5).to(be < 12)
   end
 
   it "<=" do
-	expect(5).to be <= 12
+    expect(5).to(be <= 12)
   end
 
   it "be_between inclusive" do
-	expect(5).to be_between(2,7).inclusive
+    expect(5).to(be_between(2, 7).inclusive)
   end
 
   it "be_between exclusive" do
-	expect(5).to be_between(2,7).exclusive
+    expect(5).to(be_between(2, 7).exclusive)
   end
-
 end
